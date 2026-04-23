485a8251e3f7d8b65b835e63443221fac444d85d
gaganbidaralli <161804569+gaganbidaralli@users.noreply.github.com>
2026-04-23 18:47:55 +0530

    Update initial balance for BankAccount instance
    Changed initial balance of BankAccount from 5000 to 4000.

--- BankService.java
+++ BankService.java
@@ -27,7 +27,7 @@ return balance;
 // The main entry point
 public static void main(String[] args) {
 // Create an instance of the BankAccount class
-BankAccount acc = new BankAccount(5000);
+BankAccount acc = new BankAccount(4000);
 System.out.println("Initial Balance: " + acc.getBalance());
 // Perform operations
 acc.deposit(700);
